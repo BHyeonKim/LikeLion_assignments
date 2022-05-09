f24b861fdf65e18d3513f66c4f1dc120c6771d0a
BHyeonKim <46583212+BHyeonKim@users.noreply.github.com>
2022-05-09 21:18:39 +0900

    기능구현

---
+++ fourthassignment1/src/App.js
@@ -0,0 +1,15 @@
+import Main from 'components/Main/Main';
+import Navigation from 'components/Navigation/Navigation';
+import Profile from './components/Profile/Profile';
+
+function App() {
+  return (
+    <div className='App'>
+      <Profile />
+      <Navigation />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
